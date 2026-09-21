e4d06b1857c68865a1f41bd5017fa0aa11a758d8
Jonathan DEKHTIAR <jonathan@dekhtiar.com>
2026-09-21 10:23:19 -0500

    Add support for Django 6.1 in setup.py

--- setup.py
+++ setup.py
@@ -30,6 +30,7 @@ setup(
         'Framework :: Django',
         'Framework :: Django :: 5.2',
         'Framework :: Django :: 6.0',
+        'Framework :: Django :: 6.1',
         'Development Status :: 5 - Production/Stable',
         'Programming Language :: Python :: 3.10',
         'Programming Language :: Python :: 3.11',
